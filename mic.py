ee0ecb4b919d8ec2288c9cf4ff8f0a9d2fb0ded4
Pasha Klets <pasha@klets.name>
2011-01-27 17:05:09 +0200

    second version

--- mic.py
+++ mic.py
@@ -1,102 +1,90 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import re
+
 import sys
+import re
+
+import pygtk
+pygtk.require('2.0')
+
 import gtk
-import gtk.gdk
-import gnome.ui
 import gnomeapplet
+import gobject
+
+import gnome.ui
+
 from commands import *
 
 class GnomeMicControl(gnomeapplet.Applet):
+
 	def __init__(self, applet, iid):
-		self.applet = applet
-		self.__init_core_widgets()
-		self.init_additional_widgets()
-		self.init_ppmenu()
-		self.__connect_events()
-		"""self.applet.connect("destroy", self._cleanup)"""
-		"""self.after_init()"""
-		self.applet.show_all()
+		applet = applet
 
-	def __init_core_widgets(self):
-		self.hbox = gtk.HBox()
-		self.ev_box = gtk.EventBox()
-		self.applet.add(self.hbox)
-		self.hbox.add(self.ev_box)
+		xml = '<popup name="button3"><menuitem name="About Item" verb="About" stockid="gtk-about" /></popup>'
+		verbs = [("About", self.menu)]
+		applet.setup_menu(xml, verbs, None)
 
-	def init_additional_widgets(self):
-		self.update_status()
+		hbox = gtk.VBox()
+		applet.add(hbox)
+
+		button = gtk.EventBox()
+
 		self.image = gtk.Image()
-		self.update_image()
 
-		self.button = gtk.Button()
-		self.button.connect("clicked", self.change_status)
-		self.button.set_image(self.image)
-		self.ev_box.add(self.button)
+		self.update_status()
+
+		button.connect('button-press-event', self.click)
+		button.add(self.image)
+		hbox.add(button)
+		# hbox.add(self.image)
 	
-	def update_image(self):
-		self.image.set_from_file('/server/mic/img/mic_' + self.status + '_ico.png')
+		applet.show_all()
+		gobject.timeout_add(100, self.update_status)
+
+	def click(self, widget, event):
+		if event.button == 1:
+			self.change_status()
+		elif event.button == 2:
+			self.init_menu()
+
+	def menu(self, event, data = None):
+		gnome.ui.About('GNOME Microphone Control', '0.2', 'GNU GPLv3', '', ['p01nt <pasha@klets.name>',]).show()
+
+	def change_status(self):
+		getoutput('amixer set Capture toggle')
+		self.update_status()
 
 	def update_status(self):
 		out = getoutput('amixer cget name="Capture Switch" | tail -1')
 		result = re.findall('values=(on|off),', out);
-		self.status = result[0];
+		self.image.set_from_file('/usr/share/pixmaps/mic_' + result[0] + '.png')
 
-	def change_status(self, data):
-		getoutput('amixer set Capture toggle')
-		self.update_status()
-		self.update_image()
+		return True
 
-	def init_ppmenu(self):
-		self.ppmenu_xml = """<popup name="button3"><menuitem name="About Item" verb="About" stockid="gtk-about" /></popup>"""
-		self.ppmenu_verbs = [("About", self.on_ppm_about)]
+def run_in_window():
+	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	window.set_title('Microphone Control')
+	window.connect('destroy', gtk.main_quit)
 
-	def on_ppm_about(self, event, data=None):
-		gnome.ui.About("GNOME Microphone Control", "0.1", "GNU General Public License v.2", "", ["p01nt <pasha@klets.name>, areign <areign@yandex.ru>",]
-		).show()
+	app = gnomeapplet.Applet()
+	applet_factory(app, None)
+	app.reparent(window)
+	window.show_all()
 
-	def __connect_events(self):
-		self.ev_box.connect("button-press-event", self.on_button)
-		self.button_actions = {
-			1: lambda: None,
-			2: lambda: None,
-			3: self._show_ppmenu,
-		}
+	gtk.main()
+	sys.exit()
 
-	def on_button(self, widget, event):
-		if event.type == gtk.gdk.BUTTON_PRESS:
-			self.button_actions[event.button]()
-
-	def _show_ppmenu(self):
-		self.applet.setup_menu(self.ppmenu_xml, self.ppmenu_verbs, None)
+def run_in_panel():
+	gnomeapplet.bonobo_factory('OAFIID:MicControl_Factory', GnomeMicControl.__gtype__, 'Microphone Control', '0.1', applet_factory)
 
 def applet_factory(applet, iid):
 	GnomeMicControl(applet, iid)
 	return True
 
-def run_in_window():
-	main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-	main_window.set_title("Microphone Control")
-	main_window.connect("destroy", gtk.main_quit)
-	app = gnomeapplet.Applet()
-	applet_factory(app, None)
-	app.reparent(main_window)
-	main_window.show_all()
-	gtk.main()
-	sys.exit()
-
-def run_in_panel():
-	gnomeapplet.bonobo_factory("OAFIID:MicControl_Factory", GnomeMicControl.__gtype__, "Microphone Control", "0.1", applet_factory)
-
-
-
 def main(args):
-	if len(args) == 2 and args[1] == 'debug':
+	if len(args) == 2 and args[1] == '--debug':
 		run_in_window()
 	else:
 		run_in_panel()
 
 if __name__ == '__main__':
 	main(sys.argv)
-
